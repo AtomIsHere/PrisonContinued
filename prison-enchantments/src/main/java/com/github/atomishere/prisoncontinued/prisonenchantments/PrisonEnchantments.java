1c16cdfbdebea83fc0506a90befb60d12a5d8ec8
AtomIsHere <atomishere@users.noreply.github.com>
2019-02-27 16:13:19 +1100

    Finish the token framework

--- prison-enchantments/src/main/java/com/github/atomishere/prisoncontinued/prisonenchantments/PrisonEnchantments.java
+++ prison-enchantments/src/main/java/com/github/atomishere/prisoncontinued/prisonenchantments/PrisonEnchantments.java
@@ -1,13 +1,53 @@
 package com.github.atomishere.prisoncontinued.prisonenchantments;
 
+import com.github.atomishere.prisoncontinued.prisonenchantments.tokens.TokenManager;
+import tech.mcprison.prison.PrisonAPI;
 import tech.mcprison.prison.modules.Module;
+import tech.mcprison.prison.output.Output;
+import tech.mcprison.prison.store.Collection;
+import tech.mcprison.prison.store.Database;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class PrisonEnchantments extends Module {
+
+    private Database database;
+
     /**
-     * Initialize your module.
+     * Initialize the module.
      * @param version The version of the module.
      */
     public PrisonEnchantments(String version) {
         super("Enchantments", version, 3);
+    }
+
+    @Override
+    public void enable() {
+        Optional<Database> databaseOptional = PrisonAPI.getStorage().getDatabase("tokensDb");
+        if(!databaseOptional.isPresent()) {
+            PrisonAPI.getStorage().createDatabase("tokens");
+            databaseOptional = PrisonAPI.getStorage().getDatabase("tokens");
+        }
+        this.database = databaseOptional.get();
+
+        //Load token manager
+
+        TokenManager tokenManager = new TokenManager(initCollection("tokens"));
+        try {
+            tokenManager.loadPlayers();
+        } catch(IOException ex) {
+            Output.get().logError("A player file failed to load.", ex);
+        }
+    }
+
+    private Collection initCollection(String collName) {
+        Optional<Collection> collectionOptional = database.getCollection(collName);
+        if(!collectionOptional.isPresent()) {
+            database.createCollection(collName);
+            collectionOptional = database.getCollection(collName);
+        }
+
+        return collectionOptional.orElseThrow(RuntimeException::new);
     }
 }
--- prison-enchantments/src/main/java/com/github/atomishere/prisoncontinued/prisonenchantments/tokens/TokenManager.java
+++ prison-enchantments/src/main/java/com/github/atomishere/prisoncontinued/prisonenchantments/tokens/TokenManager.java
@@ -1,13 +1,76 @@
 package com.github.atomishere.prisoncontinued.prisonenchantments.tokens;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.google.common.eventbus.Subscribe;
+import lombok.Getter;
+import tech.mcprison.prison.Prison;
+import tech.mcprison.prison.internal.events.player.PlayerJoinEvent;
+import tech.mcprison.prison.output.Output;
+import tech.mcprison.prison.store.Collection;
+import tech.mcprison.prison.store.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class TokenManager {
-    private final Map<UUID, Integer> tokenMap = new HashMap<>();
 
-    public void load() {
+    private Collection collection;
+    @Getter
+    private List<TokenPlayer> players;
 
+    public TokenManager(Collection collection) {
+        this.collection = collection;
+        this.players = new ArrayList<>();
+
+        Prison.get().getEventBus().register(this);
+    }
+
+    public void loadPlayer(String playerFile) throws IOException {
+        Document document = collection.get(playerFile).orElseThrow(IOException::new);
+        players.add(new TokenPlayer(document));
+    }
+
+    public void loadPlayers() throws IOException {
+        List<Document> players = collection.getAll();
+        players.forEach(document -> this.players.add(new TokenPlayer(document)));
+    }
+
+    public void savePlayer(TokenPlayer player, String playerFile) throws IOException {
+        collection.insert(playerFile, player.toDocument());
+    }
+
+    public void savePlayer(TokenPlayer player) throws IOException {
+        this.savePlayer(player, "tplayer_" + player.uid.getLeastSignificantBits());
+    }
+
+    public void savePlayers() throws IOException {
+        for(TokenPlayer player : players) {
+            savePlayer(player);
+        }
+    }
+
+    public Optional<TokenPlayer> getPlayer(UUID uid) {
+        return players.stream().filter(player -> player.uid.equals(uid)).findFirst();
+    }
+
+    @Subscribe
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if(!getPlayer(event.getPlayer().getUUID()).isPresent()) {
+            TokenPlayer newPlayer = new TokenPlayer();
+            newPlayer.uid = event.getPlayer().getUUID();
+            newPlayer.tokens = 0;
+
+            players.add(newPlayer);
+
+            try {
+                savePlayer(newPlayer);
+            } catch(IOException ex) {
+                Output.get().logError(
+                        "Failed to create new player data file for player " + event.getPlayer()
+                                .getName(), ex);
+            }
+        }
     }
 }
